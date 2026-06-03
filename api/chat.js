f89395e362782b6fca1c16ee59e70749b6d64214
marcusbarel-svg <marcusbarel@gmail.com>
2026-06-03 13:07:42 +0300

    Delete api directory

--- api/chat.js
+++
@@ -1,2 +0,0 @@
-
-fetch('/api/chat', {method:'POST', headers:{'Content-Type':'application/json'}, body: JSON.stringify({messages:[], memory: null})}).then(r=>r.json()).then(console.log).catch(console.error)
